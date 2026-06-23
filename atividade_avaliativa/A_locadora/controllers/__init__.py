6ca69d6e0381f9b9827da1800ec429b48d098e81
RAPHAEL_MAX_ALVES_JACOMO <22400664@ALFAF51.FLORESTA>
2026-06-23 11:31:30 -0300

    atividade locadora completo

---
+++ atividade_avaliativa/A_locadora/controllers/__init__.py
@@ -0,0 +1,2 @@
+from .locadora_controller import locadora_bp
+__all__ = [..., "locadora_bp"]
--- atividade_avaliativa/A_locadora/controllers/locadora_controller.py
+++ atividade_avaliativa/A_locadora/controllers/locadora_controller.py
@@ -11,8 +11,8 @@ locadora_bp = Blueprint("locadora", __name__, url_prefix="/locadora")
 @locadora_bp.route("/")
 def index():
     # TODO ALUNO: passe locacoes para o template
-    # locacoes = Locacao.listar_com_detalhes()
-    return render_template("locadora/lista.html", locacoes=[])
+    locacoes = Locacao.listar_com_detalhes()
+    return render_template("locadora/lista.html", locacoes=locacoes)
 
 
 @locadora_bp.route("/cadastrar", methods=["GET", "POST"])
@@ -21,8 +21,23 @@ def cadastrar():
     veiculos = Veiculo.listar()
 
     if request.method == "POST":
-        # TODO ALUNO: ler form, criar Locacao, add, commit, redirect index
-        pass
+        cliente_id = request.form.get("cliente_id")
+        veiculo_id = request.form.get("veiculo_id")
+        data_inicio = request.form.get("data_inicio")
+        data_fim = request.form.get("data_fim")
+
+        nova_locacao = Locacao(
+            cliente_id=cliente_id,
+            veiculo_id=veiculo_id,
+            data_inicio=data_inicio,
+            data_fim=data_fim,
+        )
+
+        db.session.add(nova_locacao)
+        db.session.commit()
+
+        return redirect(url_for("locadora.index"))
+        
 
     return render_template(
         "locadora/formulario.html",
